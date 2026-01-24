32a3359c195df0fe459fc434aebe35189bc52adb
sorav08 <rahulrakwal3@gmail.com>
2026-01-24 14:39:33 +0530

    classes

---
+++ Class 84 - Origin/app.js
@@ -0,0 +1,5 @@
+ const express = require("express")
+
+const app = express() //server create karna 
+
+app.listen(3000) // server start karna 
---
+++ class 86 - programming-server/server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express() //server instance cerate karna 
+app.get('/', (req,res)=>{
+    res.send("helooooo")
+})
+
+app.get("/about", (req,res)=>{
+    res.send("this is about page")
+})
+app.listen(3000)
