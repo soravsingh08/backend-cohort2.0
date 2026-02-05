7ebd832712702cae86b21842a383b47a339e8d91
sorav08 <rahulrakwal3@gmail.com>
2026-02-05 11:32:31 +0530

    note creation project and day 93 completed

--- class-93-fullstack/Backend/src/app.js
+++ class-93-fullstack/Backend/src/app.js
@@ -4,9 +4,11 @@ const express = require("express")
 const noteModel = require("./models/note.model")
 const app = express()
 const cors = require("cors")
+const path = require("path") //jis folder main ho vaha tak ka path 
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("./public")) //public karta haiscript aur html k link ko.kyunki wild ard to reject kr deta na
 
 
 //post/api/notes
@@ -63,12 +65,23 @@ app.delete("/api/notes/:id",async(req,res)=>{
    
 app.patch("/api/notes/:id",async(req,res)=>{
     const id = req.params.id
-    const {description} =req.body
+    const {title,description} =req.body
+ 
 
-    await noteModel.findByIdAndUpdate(id,{description})
+    await noteModel.findByIdAndUpdate(id,{
+        title:title,
+        description: description
+    })
 
     res.status(200).json({
         message:"Note updated successfuly"
     })
+    console.log(req.body);
+
+})
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
+    
 })
 module.exports = app
--- class-93-fullstack/Frontend/src/App.jsx
+++ class-93-fullstack/Frontend/src/App.jsx
@@ -3,23 +3,82 @@ import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState([]);
+ 
+  console.log("Hello integration")
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/api/notes")
+
+function fetchNotes(){
+  axios.get("http://localhost:3000/api/notes")
       .then((res) => {
         setNotes(res.data.notes);
       });
+}
+
+  useEffect(() => {
+    fetchNotes()
   }, []);
 
+
+  function handleSubmit(e){
+    e.preventDefault()
+  const {title,description} = e.target.elements
+  console.log(title.value,description.value)
+
+  axios.post("http://localhost:3000/api/notes",{
+    title:title.value,
+    description : description.value
+  })
+  .then(res=>{
+
+    console.log(res.data)
+    fetchNotes()
+  })
+  }
+
+  function handleDeleteNote(noteId){
+    axios.delete("http://localhost:3000/api/notes/" +noteId)
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes()
+    })
+ 
+  }
+
+  function handleUpdateNote(noteId){
+    const newTitle = prompt("Enter new title")
+        const newDescription = prompt("Enter new Description")
+    axios.patch("http://localhost:3000/api/notes/" +noteId,{
+      title:newTitle,
+      description:newDescription
+    })
+    .then(res=>{
+      console.log(res.data)
+      fetchNotes();
+      
+    })
+  }
+
+
+
   return (
+    <>
+    <form className="note-create-form" onSubmit={handleSubmit}>
+      <input name="title" type="text" placeholder="Enter Title" />
+      <input name="description" type="text" placeholder="Enter Description" />
+      <button>Create Note</button>
+     
+    </form>
     <div className="notes">
       {notes.map((note) => (
         <div className="note" key={note._id}>
           <h1>{note.title}</h1>
           <p>{note.description}</p>
+          <button onClick={()=>{handleDeleteNote(note._id)}} className="delete-btn">Delete</button>
+           <button onClick={()=>{handleUpdateNote(note._id)}} className="edit-btn">Edit</button>
         </div>
       ))}
     </div>
+        </>
   );
 }
 
