dc7488420e13fbd09a46a7f835bc3bce189f2493
sorav08 <rahulrakwal3@gmail.com>
2026-02-07 21:18:45 +0530

    login

--- class-97-authentication/src/app.js
+++ class-97-authentication/src/app.js
@@ -3,9 +3,13 @@ const authRouter =  require("./routes/auth.routes")
 const cookieParser =  require("cookie-parser")
 const app = express()
 
+//middlewares
 app.use(express.json())
-app.use("/api/auth",authRouter)
 app.use(cookieParser())
+
+//routes
+app.use("/api/auth",authRouter)
+
 
 
 module.exports = app
--- class-97-authentication/src/routes/auth.routes.js
+++ class-97-authentication/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
 
 const authRouter = express.Router()
 
@@ -15,10 +16,13 @@ authRouter.post("/register", async (req,res)=>{ //api creation - register, ye pa
       })
     }
 
+
+    const hash = crypto.createHash("md5").update(password).digest("hex")
+
     //to save this data we need model 
   //upar vale data se ek user ceate kreneg 
     const user = await userModel.create({
-      email, password, name 
+      email, password: hash, name 
     })
 
     const token = jwt.sign({
@@ -37,6 +41,49 @@ authRouter.post("/register", async (req,res)=>{ //api creation - register, ye pa
     })
    
 })
+
+/**
+ * /api/auth/protected - to access
+ */
+authRouter.post("/protected", (req,res)=>{
+  console.log(req.cookies);
+
+  res.status(200).json({
+    message : "This is a protected route",
+  })
+})
+
+/**
+ * /api/auth/login - to access
+ */
+authRouter.post("/login", async (req, res)=>{
+   const { email, password } =req.body
+   const user = await userModel.findOne({email})
+
+    if(!user){
+      return res.status(404).json({
+        message : "User not found at this email adress"
+      })
+    }
+
+    const isPasswordMatched = user.password = crypto.createHash("md5").update(password).digest("hex")
+    if(!isPasswordMatched){
+      return res.status(401).json({
+        message : "Invalid Password"
+      })
+    }
+
+    const token = jwt.sign({
+      id :user._id,
+    },process.env.JWT_SECRET)
+    
+    res.cookie("jwt_token",token)
+    res.status(200).json({
+      message:"user logged in",
+      user,
+    })
+})
+
 
 
 module.exports = authRouter
