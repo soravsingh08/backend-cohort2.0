cdbd8ba7ea95bcb25abc68e64095b94e59bbbaf8
sorav08 <rahulrakwal3@gmail.com>
2026-02-20 14:57:36 +0530

    class-103 User Feature

--- class-101-instagram-project/src/controllers/post.controller.js
+++ class-101-instagram-project/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const {toFile} = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken") //103-> to verify img kisne bheji hai throught tokens
 
 
 const imagekit = new ImageKit({
@@ -9,15 +10,118 @@ const imagekit = new ImageKit({
 async function createPostController(req,res){
     console.log(req.body,req.file)
 
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            messgae: "Token not provided, Unauthorized access"
+        })
+    }
+
+    let decoded = null 
+    try{
+           decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+    }catch(error){
+        return res.status(401).json({
+            mesasge:"user not authorized"
+        })
+    }
+
+    console.log(decoded)
+
+  
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer),'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "cohort-2-insta-clone-posts"
     })
-    res.send(file)
+    
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl:file.url,
+        user:decoded.id,
+    })
+
+    res.status(201).json({
+        message:"Post created successfully",
+        post
+    })
 }
 
+async function getPostController(req,res){
+    const token = req.cookies.token
 
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized Access"
+        })
+    }
+
+let decoded;
+try{
+decoded = jwt.verify(token, process.env.JWT_SECRET)
+}catch(err){
+    return res.status(401).json({
+        message: "Token Invalid"
+    })
+}
+const userId = decoded.id
+const posts = await postModel.find({
+    user: userId
+})
+
+res.status(200).json({
+    message: "Posts fetched successfully",
+    posts
+})
+}
+
+async function getPostDetailsController(req,res){
+    const token = req.cookies.token
+
+      if(!token){
+        return res.status(401).json({
+            message: "Unauthorized Access"
+        })
+    }
+
+    let decoded;
+
+    try{
+     decoded = jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+     message: "Invalid Token"
+        })
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    if(!post){
+        return res.status(404).json({
+            message: "Post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isUserValid){
+        return res.status(403).json({
+       message: "Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message: "Post fetched successfully",
+        post
+    })
+
+    
+}
 
 module.exports ={
-    createPostController
+    createPostController,getPostController, getPostDetailsController
 }
--- class-101-instagram-project/src/routes/post.routes.js
+++ class-101-instagram-project/src/routes/post.routes.js
@@ -12,4 +12,17 @@ const upload = multer({storage:multer.memoryStorage()})
 
 postRouter.post("/",upload.single("image"), postController.createPostController)
 
+/**
+ * GET /api/posts/ [protected]
+ */
+postRouter.get("/",postController.getPostController)
+
+/**
+ * GET /api/posts/details/:postid
+ * - return an detail avout specific post with the id. 
+ * also check weather the post belong to the user that the request come from
+ */
+
+postRouter.get("/details/:postId",postController.getPostDetailsController)
+
 module.exports = postRouter
