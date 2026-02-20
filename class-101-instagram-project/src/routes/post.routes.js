c7797b7a5ca138017c972739cd844fdee35ff078
sorav08 <rahulrakwal3@gmail.com>
2026-02-20 10:22:24 +0530

    class-102 imagekit and multer

--- class-101-instagram-project/src/routes/post.routes.js
+++ class-101-instagram-project/src/routes/post.routes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const postRouter =express.Router()
 const postController = require("../controllers/post.controller")
 const multer =  require("multer")
+const upload = multer({storage:multer.memoryStorage()})
 
 
 /**
@@ -9,6 +10,6 @@ const multer =  require("multer")
  * - req.body ={caption,image-file}
  */
 
-postRouter.post("/", postController.createPostController)
+postRouter.post("/",upload.single("image"), postController.createPostController)
 
 module.exports = postRouter
